3e6de81abce51bd44f988c6071eed833737e651b
olegbergin <oleg.bragin.01@gmail.com>
2025-06-20 21:19:18 +0300

    db updated

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2025 at 07:14 PM
+-- Generation Time: Jun 20, 2025 at 08:14 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
